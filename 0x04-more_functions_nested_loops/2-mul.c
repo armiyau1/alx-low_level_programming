cbd9c8283eeac119ec77b0c65c3c7d9f4f2bb959
armiyau1 <yauarmi688@gmail.com>
2023-02-23 05:34:44 -0800

    mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - return the product of ist parameters
+ * @a: input type numbwr
+ * @b: input type number
+ * Return: 0
+ */
+int mull(int a, int b)
+{
+return (a * b);
+}
