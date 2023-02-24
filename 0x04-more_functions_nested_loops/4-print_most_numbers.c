83e6696933b42228b3cb66310f217c0ce63a5e01
armiyau1 <yauarmi688@gmail.com>
2023-02-24 00:03:39 -0800

    digit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * print_most_numbers - print numbesr
  * Return: 0
@@ -7,13 +6,10 @@
 void print_most_numbers(void)
 {
 char i = 9;
-
 while (i <= 9)
 {
 if (i != 2 && i != 4)
-{ 
 _putchar (i + '0');
-}
 i++;
 }
 _putchar ('\n');
